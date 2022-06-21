ea77ca814592043bf309b1d61956c51bf9e97c8f
undertayker <andrey.kozlovskiy99@mail.ru>
2022-06-21 23:22:35 +0300

    new

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercise_2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = " Привет ";
+            string mentor = " ментор ";
+            string str = " меня зовут " + " Андрей";
+            int age = 23;
+            double weight = 70;
+            int gold = 20000;
+            int time = 22;
+            string car = " bmw ";
+            string color = " синий ";
+            int year = 2016;
+            int speed = 240;
+
+            Console.WriteLine("привет");
+            Console.WriteLine("ментор");
+            Console.WriteLine(str);
+            Console.WriteLine($"Возраст: {age}");
+            Console.WriteLine($"вес: {weight}");
+            Console.WriteLine($"золото: {gold}");
+            Console.WriteLine($"время: {time}");
+            Console.WriteLine($"машина: {car}");
+            Console.WriteLine($"год: {year}");
+            Console.WriteLine($"цвет: {color}");
+            Console.WriteLine($"скорость: {speed}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+}
