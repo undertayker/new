3152f995726c1b8e4e919051726abbbef5e61fcb
undertayker <andrey.kozlovskiy99@mail.ru>
2022-06-22 14:34:39 +0300

    Create Program2.cs

---
+++ Program2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Exercise_4
+{
+
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            string year;
+            string zodiacSign;
+            string work;
+
+            Console.WriteLine("Введите свое имя : ");
+            myName = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет : ");
+            year = Console.ReadLine();
+            Console.WriteLine("Ваш знак зодиака : ");
+            zodiacSign = Console.ReadLine();
+            Console.WriteLine("Где вы работаете : ");
+            work = Console.ReadLine();
+            Console.WriteLine("Вас зовут : " + myName);
+            Console.WriteLine("Вам : " + year + "год");
+            Console.WriteLine("Вы : " + zodiacSign);
+            Console.WriteLine("Вы работаете : " + work);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+}
