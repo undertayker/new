d1b82854c65fae561ce81a3f3c86f46c77bd7331
undertayker <andrey.kozlovskiy99@mail.ru>
2022-06-23 13:05:10 +0300

    Create Program6.cs

---
+++ Program6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class Program6
+    {
+        static void Main(string[] args)
+        {
+            int timeOfReceipt = 10;
+            Console.WriteLine("Введите количество людей в очереди : ");
+            int peoply = int.Parse(Console.ReadLine());
+            int a = timeOfReceipt;
+            int b = peoply;
+            int time = a * b;
+            int minutes = 60;
+            int hour;
+
+            Console.WriteLine("Время ожидания");
+            Console.WriteLine("Осталось часов\t:{0}", time / minutes );
+            Console.WriteLine("Осталось минут\t:{0}", time % minutes );
+        }
+    }
+}
